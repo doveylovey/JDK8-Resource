575e6531287a78bd4c9861e7472b5884c1bfac21
Administrator <1135782208@qq.com>
2019-10-14 22:42:17 +0800

    Initial commit

---
+++ src/test/java/com/resource/test/StringTest.java
@@ -0,0 +1,40 @@
+package com.resource.test;
+
+import org.junit.Test;
+
+/**
+ * @Description: String类的测试用例
+ * @Author: TeGongX
+ * @Email: 1135782208@qq.com
+ * @Date: 2019年10月13日
+ * @Version: V1.0.0
+ * @Copyright: Copyright (c) 2019
+ */
+public class StringTest {
+    @Test
+    public void test01() {
+        String str1 = new String("123");
+        String str2 = "123";
+        String str3 = "123";
+        String str4 = str1.intern();
+        System.out.println(str1 == str2);// false：str1在堆中创建对象，str2在常量池中创建对象
+        System.out.println(str2 == str3);// true：str2在常量池中创建对象，str3直接返回的str2创建的对象的引用，所以str2和str3指向常量池中同一个对象
+        System.out.println(str4 == str3);// true：str4返回常量池中值为"123"的对象，因此str4和str2、str3都相等
+    }
+
+    public static final String X = "ABC";// 常量X
+
+    @Test
+    public void test02() {
+        String str5 = new String("ABC");
+        String str6 = str5 + "DEF";// 堆中创建
+        String str7 = "ABC" + "DEF";// 常量池
+        String str8 = X + "DEF";// X为常量,值是固定的,因此X+"DEF"值已经定下来为ABCDEF，实际上编译后得代码相当于String str8 = "ABCDEF"
+        String str9 = "ABC";
+        String str10 = str9 + "DEF";// 堆中
+        System.out.println(str6 == str7);// false
+        System.out.println(str8 == str7);// true
+        System.out.println(str10 == str7);// false
+        System.out.println(X == str9);// true
+    }
+}
